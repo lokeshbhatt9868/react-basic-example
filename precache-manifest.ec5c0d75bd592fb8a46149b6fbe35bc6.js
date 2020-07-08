adf13daf26a26315ab2df6067fa47b99a38a887b
Lokesh Bhatt <lokesh.bhatt@tavant.com>
2020-07-08 09:56:30 +0530

    Updates

---
+++ precache-manifest.ec5c0d75bd592fb8a46149b6fbe35bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c944283a0cbfea700ff33c42515af16",
+    "url": "/react-basic-example/index.html"
+  },
+  {
+    "revision": "1af735aed7e813bf9fe7",
+    "url": "/react-basic-example/static/css/main.bf2b281d.chunk.css"
+  },
+  {
+    "revision": "5f04277db70e3a9bee79",
+    "url": "/react-basic-example/static/js/2.f6a2a328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-example/static/js/2.f6a2a328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af735aed7e813bf9fe7",
+    "url": "/react-basic-example/static/js/main.39a89cd6.chunk.js"
+  },
+  {
+    "revision": "63494a727989cd13fbf0",
+    "url": "/react-basic-example/static/js/runtime-main.417965eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basic-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
